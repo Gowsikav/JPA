9af4dd0f333f52744608eb52ac6e52892ee405ee
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-09-03 17:54:49 +0530

    complete task: add dependency for application configuration and implemented

--- online-food-order/src/main/java/com/xworkz/foodorder/config/FoodOrderConfiguration.java
+++ online-food-order/src/main/java/com/xworkz/foodorder/config/FoodOrderConfiguration.java
@@ -1,17 +1,30 @@
 package com.xworkz.foodorder.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 @ComponentScan("com.xworkz.foodorder")
 @EnableWebMvc
+@PropertySource("classpath:application.properties")
 public class FoodOrderConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
 
     public FoodOrderConfiguration()
     {
@@ -32,5 +45,38 @@ public class FoodOrderConfiguration implements WebMvcConfigurer {
         internalResourceViewResolver.setPrefix("/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean containerEntityManagerFactoryBean()
+    {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean=new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter=new HibernateJpaVendorAdapter();
+        entityManagerFactoryBean.setJpaVendorAdapter(hibernateJpaVendorAdapter);
+        entityManagerFactoryBean.setJpaProperties(getHibernateProperties());
+        entityManagerFactoryBean.setPackagesToScan(new String[]{"com.xworkz.library.entity"});
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource()
+    {
+        DriverManagerDataSource dataSource=new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+        return dataSource;
+    }
+
+    public Properties getHibernateProperties()
+    {
+        Properties properties=new Properties();
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
+        properties.setProperty("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
+        return properties;
     }
 }
